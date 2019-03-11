5206ff5c47ad40cef87a9e3a5854f54534a9a8a0
Zane Campbell <zanegitinit@gmail.com>
2019-03-10 18:52:42 -0700

    static html and JSON data through routes added

--- src/app.js
+++ src/app.js
@@ -3,19 +3,26 @@ const express = require('express');
 const app = express();
 
 app.get('', (req, res) => {
-    res.send('Hello Express!')
+    res.send('<h1>Weather</h1>')
 });
 
 app.get('/help', (req, res) => {
-    res.send('Help page');
+    res.send([{
+        name: 'Zane'
+    }, {
+        name: 'Andrew'
+    }]);
 });
 
 app.get('/about', (req, res) => {
-    res.send('About page');
+    res.send('<h1>About</h1>');
 })
 
 app.get('/weather', (req, res) => {
-    res.send('Weather page');
+    res.send({
+        forecast: 'Sunny all day',
+        location: 'Seattle'
+    });
 });
 
 app.listen(3000, () => {
