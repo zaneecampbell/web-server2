2d45ec934842620a47100e52bdc5a3db86ce6b5c
Zane Campbell <zanegitinit@gmail.com>
2019-03-20 14:53:26 -0700

    setup input form for dynamic weather search

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,14 @@
 console.log('Client side javascript is loaded')
 
-fetch('http://localhost:3000/weather?address=seattle').then((response) => {
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const location = search.value
+
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
     response.json().then((data) => {
         if (data.error) {
             console.log(data.error);
@@ -10,3 +18,4 @@ fetch('http://localhost:3000/weather?address=seattle').then((response) => {
         }
     })
 })
+});
