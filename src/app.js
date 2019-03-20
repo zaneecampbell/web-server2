5fffdd4274ea42cda87b1fcaea6003a766b5cacd
Zane Campbell <zanegitinit@gmail.com>
2019-03-19 18:14:40 -0700

    set up query string for weather app

--- src/app.js
+++ src/app.js
@@ -40,9 +40,29 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'Address required'
+        });
+    }
+
     res.send({
         forecast: 'Sunny all day',
-        location: 'Seattle'
+        location: 'Seattle',
+        address: req.query.address
+    });
+});
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        });
+    }
+
+    console.log(req.query.search)
+    res.send({
+        products: []
     });
 });
 
