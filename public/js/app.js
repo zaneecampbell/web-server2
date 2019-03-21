9fe9082ce0ea5198951cffa93ffe699626c42c82
Zane Campbell <zanegitinit@gmail.com>
2019-03-21 03:00:12 -0700

    site shows data in plain text after search

--- public/js/app.js
+++ public/js/app.js
@@ -2,19 +2,26 @@ console.log('Client side javascript is loaded')
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+// messageOne.textContent = 'From JS'
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const location = search.value
 
+    messageOne.textContent = 'Loading...';
+
     fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
     response.json().then((data) => {
         if (data.error) {
-            console.log(data.error);
+            messageOne.textContent = data.error
+            messageTwo.textContent = ''
         } else {
-            console.log(data.location);
-            console.log(data.forecast);
+            messageOne.textContent = data.location
+            messageTwo.textContent = data.forecast
         }
     })
 })
